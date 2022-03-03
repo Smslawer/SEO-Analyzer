ac71156f142bc03eac2318c7be40e4ddf5732a4b
UshakovAlexey <alexey.ushakov2018@yandex.ru>
2022-03-03 10:36:10 +0500

    Ready to deploy

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,11 +1,28 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
+
+
 public class App {
     public static void main(String[] args) {
         System.out.println(returnHello());
+
+        Javalin app = getApp();
+        app.start(5000);
     }
 
     public static String returnHello() {
         return "Hello";
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(JavalinConfig::enableDevLogging);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
     }
 }
